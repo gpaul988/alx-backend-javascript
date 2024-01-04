16b322dde97c4ef40edcc388dfd5818eaca5793b
gpaul988 <gpaul988@gmail.com>
2024-01-04 05:13:06 -0800

    bug remove from 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,8 +4,6 @@ import ClassRoom from './0-classroom';
  * Author - Graham S. Paul
  * 1-make_classrooms.js
  */
-export default () => {
-  new ClassRoom(19);
-  new ClassRoom(20);
-  new ClassRoom(34);
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
