8dbb5e52532f0aa3117c354af52f51183ca427c8
gpaul988 <gpaul988@gmail.com>
2024-01-08 11:47:33 -0800

    bug removed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,7 @@ export default function cleanSet(set, startString) {
   const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
-  }
-/*Graham S. Paul - 8-clean_set.js*/  
+  }  
 for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
@@ -15,3 +14,4 @@ for (const value of set.values()) {
   }
   return parts.join('-');
 }
+/* Graham S. Paul - 8-clean_set.js */ 
