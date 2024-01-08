0463c1c0331287bb714047fc059a8b6e6b5d453c
gpaul988 <gpaul988@gmail.com>
2024-01-08 11:59:32 -0800

    bug removed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,8 @@ export default function cleanSet(set, startString) {
   const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
-  }  
-for (const value of set.values()) {
+  }
+  for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
