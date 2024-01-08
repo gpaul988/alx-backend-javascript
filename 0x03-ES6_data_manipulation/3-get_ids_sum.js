a2891bcc6bb4c8caec3721ba8643c5c091aa163e
gpaul988 <gpaul988@gmail.com>
2024-01-08 11:16:07 -0800

    bug removed

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,7 @@
+/**
+ * Author: Graham S. Paul
+ * 3-get_ids_sum.js
+ */
 export default function getStudentIdsSum(students) {
   if (Array.isArray(students)) {
     return students.reduce((accumulator, each) => accumulator + each.id, 0);
