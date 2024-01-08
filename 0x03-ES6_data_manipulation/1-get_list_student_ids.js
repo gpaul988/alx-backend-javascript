6cd0b80ef23771ed835a4dd4f2d37d773172d929
gpaul988 <gpaul988@gmail.com>
2024-01-08 11:12:54 -0800

    bug removed

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,7 @@
+/**
+ * Author: Graham S. Paul
+ * 1-get_list_student_ids.js
+ */
 export default function getListStudentIds(students) {
   if (Array.isArray(students)) {
     return students.map((items) => items.id);
