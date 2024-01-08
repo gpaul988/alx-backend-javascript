82bbbfd0390df4ac56fe97b95b2ed78ea751f430
gpaul988 <gpaul988@gmail.com>
2024-01-08 11:14:24 -0800

    bug removed

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,7 @@
+/**
+ * Author: Graham S. Paul
+ * 2-get_students_by_loc.js
+ */
 export default function getStudentsByLocation(students, city) {
   if (Array.isArray(students)) {
     return students.filter((items) => items.location === city);
