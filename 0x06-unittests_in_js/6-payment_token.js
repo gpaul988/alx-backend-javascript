0fe1a6a7e3888990d87a82774b68da05ca3bb6c0
gpaul988 <gpaul988gmail.com>
2024-02-29 20:22:25 +0100

    payment token added

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,3 +1,11 @@
+/**
+ * This function is a mock of an API call to get a payment token
+ * @param {boolean} success - Indicates if the call should be successful
+ * @returns {Promise} - The response from the API
+ * @example getPaymentTokenFromAPI(true).then(console.log).catch(console.log);
+ * @example getPaymentTokenFromAPI(false).then(console.log).catch(console.log);
+ * @author Graham S. Paul
+ */
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
   if (success) {
     resolve({data: 'Successful response from the API'});
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,6 +1,14 @@
-const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
 
+/**
+ * This function is a mock of an API call to get a payment token
+ * @param {boolean} success - Indicates if the call should be successful
+ * @returns {Promise} - The response from the API
+ * @example getPaymentTokenFromAPI(true).then(console.log).catch(console.log);
+ * @example getPaymentTokenFromAPI(false).then(console.log).catch(console.log);
+ * @author Graham S. Paul
+ */
 describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
